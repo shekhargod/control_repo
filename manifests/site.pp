6830ab842e541cbcf17a368d349cc3cb6feaa5f5
shekhargod <shekhargod@yahoo.com>
2019-02-28 12:42:24 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'this is readme',
-    owner   => 'root',
-  }
+ 
 }
 node 'master.puppet.vm'{
   include role::master_server
+   file {'/root/README':
+    ensure  => file,
+    content => $fqdn,
+  }
 }
 
 node /^web/ {
