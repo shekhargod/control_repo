b89c55d926ac28aa1c5c9184178f0f610837ff86
shekhargod <shekhargod@yahoo.com>
2019-02-27 21:13:20 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   file {'/root/README':
-  ensure  => file,
-  content => 'this is readme',
-  owner   => 'root',
-
+    ensure  => file,
+    content => 'this is readme',
+    owner   => 'root',
+  }
 }
